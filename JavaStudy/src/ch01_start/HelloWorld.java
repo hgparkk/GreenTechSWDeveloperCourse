d6f3def31b13f0a3cac81bfbb62e51d49049e110
phg2559 <phg2559@naver.com>
2024-06-27 15:08:16 +0900

    first commit

--- JavaStudy/src/ch01_start/HelloWorld.java
+++ JavaStudy/src/ch01_start/HelloWorld.java
@@ -3,5 +3,8 @@ package ch01_start;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
+		
+		//syso 입력 후 + ctrl + space : System.out.println 자동완성
+		System.out.println(30);
 	}
 }
---
+++ JavaStudy/src/ch02_variable/VariableMain.java
@@ -0,0 +1,14 @@
+package ch02_variable;
+
+public class VariableMain {
+	
+	//main 입력 후 ctrl + space : public static void main(String[] args) 완성
+	public static void main(String[] args) {
+		
+		int money;
+		
+		money = 100000;
+		
+		System.out.println(money);
+	}
+}
