9479bfba4545e16f112ed5902906fed2b0509c45
phg2559 <phg2559@naver.com>
2024-06-27 17:52:03 +0900

    new commit

---
+++ JavaStudy/src/ch02_variable/StringMain.java
@@ -0,0 +1,58 @@
+package ch02_variable;
+
+public class StringMain {
+	public static void main(String[] args) {
+
+		// 문자 타입
+		char A = 65; // char 하나의 문자를 담는데 사용.
+
+		System.out.println(A); // 유니코드 65는 A이다.
+
+		char ga = '가'; // '가'에 대한 유니 코드인 44032 숫자가 저장됨.
+
+		System.out.println(ga); // 표기는 유니 코드에 해당하는 문자가 보여진다.
+
+		// 문자열 타입
+		String fruits = "Apple, Banana, Cherry";
+		System.out.println(fruits.length()); // 문자열의 길이를 반환.
+		System.out.println(fruits.charAt(7)); // 인덱스 번호에 해당하는 문자를 반환.(char 타입)
+
+		// subStirng(시작 인덱스, 끝 인덱스) : 해당 문자열을 시작 인덱스부터 끝 인덱스 전까지 반환.(String 타입)
+		System.out.println(fruits.substring(0, 5));
+		System.out.println(fruits.substring(7, 13));
+		System.out.println(fruits.substring(15, 21));
+
+		// substring(시작 인덱스) : 해당 문자열을 시작 인덱스부터 끝까지 반환.(String 타입)
+		System.out.println(fruits.substring(7));
+
+		// fruits.substring(99); // 존재하지 않는 인덱스 번호를 넣으면 에러가 발생함.
+
+		System.out.println(fruits.replace("Apple", "Avocado")); // 해당 문자열에서 특정 문자열을 바꾼 결과를 반환.
+
+		System.out.println(fruits); // 변수는 변하지 않는다.
+
+		fruits = fruits.replace("Apple", "Avocado"); // 원본 문자열 교체하기.
+		System.out.println(fruits);
+
+		String example = "   Hello, Master !!  ";
+		System.out.println(example.trim()); // 해당 문자열의 양 끝에 존재하는 모든 공백문자가 제거된 문자열을 반환.
+		System.out.println(example.replace(" ", "")); // replace를 이용한 공백 제거.
+
+		System.out.println(fruits.toUpperCase()); // 문자열을 모두 대문자로 바꾼 결과를 반환.
+		System.out.println(fruits.toLowerCase()); // 문자열을 모두 소문자로 바꾼 결과를 반환.
+
+		System.out.println(fruits.indexOf("Banana")); // 해당 문자열에서 괄호 안에 있는 문자열의 인덱스 번호 반환.
+		System.out.println(fruits.indexOf("Orange")); // 존재하지 않는 문자열이면 -1 반환.
+		System.out.println(fruits.indexOf("a")); // 대소문자를 구별하고 가장 앞에있는 문자열의 인덱스를 반환.
+
+		System.out.println(fruits.contains("Banana")); // 해당 문자열 내부 괄호안 문자열이 존재하면 true, 존재하지 않으면 false 반환.
+		System.out.println(fruits.contains("Orange"));
+
+		System.out.println(fruits + ", Durian"); // 문자열 이어붙이기.
+		fruits = fruits + ", Durian";
+		System.out.println(fruits);
+
+		// etc
+		System.out.println("Mango, " + fruits);
+	}
+}
--- JavaStudy/src/ch02_variable/VariableMain.java
+++ JavaStudy/src/ch02_variable/VariableMain.java
@@ -1,14 +1,41 @@
 package ch02_variable;
 
 public class VariableMain {
-	
-	//main 입력 후 ctrl + space : public static void main(String[] args) 완성
+
+	// main 입력 후 ctrl + space : public static void main(String[] args) 완성
 	public static void main(String[] args) {
-		
+
+		// 변수
 		int money;
-		
+
 		money = 100000;
-		
+
 		System.out.println(money);
+
+		money = 200000;
+
+		System.out.println(money);
+
+		// 상수
+		final int AGE = 20;
+
+		System.out.println(AGE);
+
+		// age = 30; 불가능
+
+		int number = 5;
+
+		// 정수 타입
+		byte byteNumber = 100; // 1바이트
+		short shortNumber = 100; // 2바이트
+		int intNumber = 1000000000; // 4바이트
+		long longNumber = 100000000000L; // 8바이트, 숫자 뒤에 L을 붙여주어야 함.
+
+		// 실수 타입
+		float floatNumber = 13.45f; // 4바이트, 숫자 뒤에 f를 붙여주어야 함.
+		double doubleNumber = 13.45; // 8바이트
+
+		// 불리언 타입
+		boolean isRun = true;
 	}
 }
