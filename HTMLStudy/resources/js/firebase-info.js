851d22039bfc72916f485878b75ccc7b6b9778a2
phg2559 <phg2559@naver.com>
2024-08-14 12:11:42 +0900

    new commit

--- HTMLStudy/resources/js/firebase-info.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBivlire0dN3GRPJDDjLcjdvFHSfk_nRI4",
-  authDomain: "fir-study-d3394.firebaseapp.com",
-  projectId: "fir-study-d3394",
-  storageBucket: "fir-study-d3394.appspot.com",
-  messagingSenderId: "789548881190",
-  appId: "1:789548881190:web:f7592d6d3bf8ed05ea9de7",
-  measurementId: "G-VVJGH8YKMB"
-};
