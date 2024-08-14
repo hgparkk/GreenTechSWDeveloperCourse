26f30daa693feae7833655557dc439dd6e8aa63d
phg2559 <phg2559@naver.com>
2024-08-14 13:56:28 +0900

    back new commit

---
+++ HTMLStudy/resources/js/serviceKey-info.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBivlire0dN3GRPJDDjLcjdvFHSfk_nRI4",
+  authDomain: "fir-study-d3394.firebaseapp.com",
+  projectId: "fir-study-d3394",
+  storageBucket: "fir-study-d3394.appspot.com",
+  messagingSenderId: "789548881190",
+  appId: "1:789548881190:web:f7592d6d3bf8ed05ea9de7",
+  measurementId: "G-VVJGH8YKMB"
+};
+
+const servicekeyEncoding = "?serviceKey=7iwd9W8L5Kbf0TSMmZlfTf9B4Bzi6b6KX5HkUGG0XkD19ZSr%2BIw3WD9zmD1MoXdQ7UQhPYlBK3wWkg3%2B4jSaVQ%3D%3D"
+
+const servicekeyDecoding = "?serviceKey=7iwd9W8L5Kbf0TSMmZlfTf9B4Bzi6b6KX5HkUGG0XkD19ZSr+Iw3WD9zmD1MoXdQ7UQhPYlBK3wWkg3+4jSaVQ=="
