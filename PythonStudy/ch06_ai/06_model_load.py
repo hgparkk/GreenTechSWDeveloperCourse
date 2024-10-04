caf0bfbaeb9572935637d7a0519cf416e90bf08f
phg2559 <phg2559@naver.com>
2024-10-04 16:04:56 +0900

    new commit

--- PythonStudy/ch06_ai/06_model_load.py
+++ PythonStudy/ch06_ai/06_model_load.py
@@ -6,3 +6,5 @@ model = joblib.load("../models/diabetes_model.pkl")
 x = np.array([[0, 85, 80, 29, 0, 26.6, 0.351, 31]])
 y_pred = model.predict_proba(x)
 print("당뇨병에 걸릴 확률 : ", round(y_pred[0][1] * 100, 1), "%")
+
+print("a")
